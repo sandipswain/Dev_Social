b02509080db6f722187bcf33d9f0b69cbf2924c3
Sandip Swain <swainsandip9@gmail.com>
2022-01-07 09:18:21 +0530

    Done with the responsive sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, makeStyles } from "@material-ui/core";
+import { Post } from "./Post";
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
@@ -7,5 +8,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 export const Feed = () => {
   const classes = useStyles();
-  return <Container className={classes.container}>Feed</Container>;
+  return (
+    <Container className={classes.container}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Container>
+  );
 };
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(10),
+  },
+}));
+export const Post = () => {
+  const classes = useStyles();
+  return <>hello</>;
+};
