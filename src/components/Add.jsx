aa09765cb46973279f830fc2580b155f36ac75ec
Sandip Swain <swainsandip9@gmail.com>
2022-01-09 19:47:12 +0530

    Added Tooltip

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core";
-const useStyles = makeStyles((theme) => ({}));
+import React, { useState } from "react";
+import { makeStyles, Tooltip, Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "fixed",
+    bottom: 20,
+    right: 10,
+  },
+}));
 export const Add = () => {
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
-  return <>Add</>;
+  return (
+    <>
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
+        <Fab color="primary" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  );
 };
