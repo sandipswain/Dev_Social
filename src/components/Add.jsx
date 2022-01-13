9fc78255a905e18ba2d1ba75024629fef4cd986f
Sandip Swain <swainsandip9@gmail.com>
2022-01-13 08:30:07 +0530

    Did the form for the Add component

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
-import { makeStyles, Tooltip, Fab, Modal, Container } from "@material-ui/core";
+import {
+  makeStyles,
+  Tooltip,
+  Fab,
+  Modal,
+  Container,
+  TextField,
+  MenuItem,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormLabel,
+  Button,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 const useStyles = makeStyles((theme) => ({
   fab: {
@@ -22,6 +35,12 @@ const useStyles = makeStyles((theme) => ({
       height: "100vh",
     },
   },
+  form: {
+    padding: theme.spacing(2),
+  },
+  item: {
+    marginBottom: theme.spacing(3),
+  },
 }));
 export const Add = () => {
   const [open, setOpen] = useState(false);
@@ -34,7 +53,79 @@ export const Add = () => {
         </Fab>
       </Tooltip>
       <Modal open={open}>
-        <Container className={classes.container}>My Modal</Container>
+        <Container className={classes.container}>
+          <form className={classes.form} autoComplete="off">
+            <div className={classes.item}>
+              <TextField
+                id="standard-basic"
+                label="Title"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField
+                id="outlined-multiline-static"
+                label="Description"
+                multiline
+                rows={4}
+                defaultValue="Tell your story..."
+                variant="outlined"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+              <FormLabel component="legend">Who can comment?</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="Everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="My Friends"
+                  control={<Radio size="small" />}
+                  label="My Friends"
+                />
+                <FormControlLabel
+                  value="Nobody"
+                  control={<Radio size="small" />}
+                  label="Nobody"
+                />
+                <FormControlLabel
+                  value="Custom"
+                  disabled
+                  control={<Radio size="small" />}
+                  label="Custom (Premium)"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.item}>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginRight: 10 }}
+              >
+                Create
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Container>
       </Modal>
     </>
   );
