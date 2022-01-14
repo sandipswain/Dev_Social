85149f9bf5165383fccc4650b23fb096438d2ab0
Sandip Swain <swainsandip9@gmail.com>
2022-01-14 12:41:47 +0530

    Done with the RightBar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,11 +1,126 @@
 import React from "react";
-import { Container, makeStyles } from "@material-ui/core";
+import {
+  Avatar,
+  Container,
+  Divider,
+  ImageList,
+  ImageListItem,
+  Link,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { AvatarGroup } from "@material-ui/lab";
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+    position: "sticky",
+    top: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#555",
+  },
+  avatar: {
+    marginBottom: 20,
+  },
+  link: {
+    marginRight: theme.spacing(2),
+    textDecoration: "none",
+    color: "#555",
+    fontSize: 16,
   },
 }));
 export const Rightbar = () => {
   const classes = useStyles();
-  return <Container className={classes.container}>Rightbar</Container>;
+  return (
+    <Container className={classes.container}>
+      <Typography className={classes.title} gutterBottom>
+        Friends
+      </Typography>
+
+      <AvatarGroup max={6} className={classes.avatar}>
+        <Avatar
+          alt="Remy Sharp"
+          src="https://mui.com/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          alt="Travis Howard"
+          src="https://mui.com/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          alt="Cindy Baker"
+          src="https://mui.com/static/images/avatar/3.jpg"
+        />
+        <Avatar alt="Agnes Walker" src="" />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://mui.com/static/images/avatar/5.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://mui.com/static/images/avatar/6.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://mui.com/static/images/avatar/7.jpg"
+        />
+      </AvatarGroup>
+      <Typography className={classes.title} gutterBottom>
+        Ads by Dev
+      </Typography>
+      <ImageList rowHeight={100} style={{ marginBottom: 20 }} cols={3}>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/breakfast.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/burgers.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/camera.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/morning.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/mushroom.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/hats.jpg"
+            alt=""
+          />
+        </ImageListItem>
+      </ImageList>
+      <Typography gutterBottom>Categories</Typography>
+      <Link href="#" className={classes.link} variant="body2">
+        Sport
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Food
+      </Link>
+      <Divider flexItem style={{ marginBottom: 5 }} />
+      <Link href="#" className={classes.link} variant="body2">
+        Movies
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Current Events
+      </Link>
+    </Container>
+  );
 };
